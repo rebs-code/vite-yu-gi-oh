570ff054ddabb01a6ffd78a6fd43d0e294998cb4
rebs-code <jack.rebussi@gmail.com>
2023-12-12 15:53:31 +0100

    add: card component and api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,24 @@
 <script>
+import axios from 'axios';
 import MainSelect from './MainSelect.vue';
+import { store } from '../store';
 export default {
     name: 'AppMain',
     components: {
         MainSelect,
+    },
+    data() {
+        return {
+            store,
+            cards: [],
+            apiURL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=39&offset=0',
+        };
+    },
+    created() {
+        axios.get(this.apiURL)
+            .then((response) => {
+                this.cards = response.data.data;
+            })
     },
 };
 </script>
@@ -12,6 +27,10 @@ export default {
     <main class="app-main">
         <div class="container">
             <MainSelect />
+            <div>
+                {{ cards }}
+
+            </div>
         </div>
     </main>
 </template>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: 'CardComponent',
+};
+</script>
+
+<template></template>
+
+<style scoped lang="scss"></style>
