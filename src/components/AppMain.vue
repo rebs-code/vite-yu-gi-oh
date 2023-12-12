9e376c04feec790edbf7ffa12929d388ee22e195
rebs-code <jack.rebussi@gmail.com>
2023-12-12 15:24:17 +0100

    feat: select component to main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,17 @@
 <script>
+import MainSelect from './MainSelect.vue';
 export default {
     name: 'AppMain',
+    components: {
+        MainSelect,
+    },
 };
 </script>
 
 <template>
     <main class="app-main">
         <div class="container">
-
+            <MainSelect />
         </div>
     </main>
 </template>
---
+++ src/components/MainSelect.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'MainSelect',
+};
+</script>
+
+<template>
+    <div class="select-div col-2 py-5">
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
